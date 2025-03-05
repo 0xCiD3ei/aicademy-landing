800e084008135252d5f6f2a893f1977382b48a94
duytiennnn <tien.nguyen@icetea.io>
2025-03-05 17:52:07 +0700

    refactor: config tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,23 +6,36 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        primary: ['Poppins', ...defaultTheme.fontFamily.sans],
+        'dela-gothic-one': ['Dela Gothic One', ...defaultTheme.fontFamily.sans],
+        'comic-sans-ms': ['Comic Sans MS', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
-          // Customize it on globals.css :root
-          50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+          DEFAULT: 'rgb(var(--tw-color-primary))',
+          foreground: 'rgb(var(--tw-color-primary-foreground))',
         },
+        secondary: {
+          DEFAULT: 'rgb(var(--tw-color-secondary))',
+          foreground: 'rgb(var(--tw-color-secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'rgb(var(--tw-color-destructive))',
+          foreground: 'rgb(var(--tw-color-destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'rgb(var(--tw-color-muted))',
+          foreground: 'rgb(var(--tw-color-muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'rgb(var(--tw-color-accent))',
+          foreground: 'rgb(var(--tw-color-accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'rgb(var(--tw-color-popover))',
+          foreground: 'rgb(var(--tw-color-popover-foreground))',
+        },
+        border: 'rgb(var(--tw-color-border))',
         dark: '#222222',
       },
       keyframes: {
