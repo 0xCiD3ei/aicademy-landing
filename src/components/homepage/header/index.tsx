40d2145aa4a53b2f513ede1c3cdd743d6d368077
duytiennnn <tien.nguyen@icetea.io>
2025-03-05 17:56:51 +0700

    feat: add header UI

---
+++ src/components/homepage/header/index.tsx
@@ -0,0 +1,23 @@
+import Banner from '@/components/homepage/header/Banner';
+
+import Logo from '@/assets/homepage/logo.svg';
+
+export default function Header() {
+  return (
+    <>
+      <header className='mt-5 px-[72px] w-full h-[92px] flex items-center justify-center'>
+        <div className='bg-secondary px-5 rounded-xl flex items-center justify-between h-full w-full'>
+          <div>
+            <Logo className='w-[220px] h-[52px]' />
+          </div>
+          <div className='text-base text-white font-semibold bg-primary rounded-lg px-[29px] py-2.5 drop-shadow-[3px_3px_0px_rgba(198,198,198,1)]'>
+            Login
+          </div>
+        </div>
+      </header>
+      <div className='mt-[50px]'>
+        <Banner />
+      </div>
+    </>
+  );
+}
